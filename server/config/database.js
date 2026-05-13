e799d8d23070fcab023f685f5d989f565c7296a7
unknown <nicomanalang12@gmail.com>
2026-05-13 18:00:57 +0800

    fix: use Railway volume for persistent database

--- server/config/database.js
+++ server/config/database.js
@@ -4,21 +4,16 @@ const path = require('path');
 const fs = require('fs');
 
 function runMigrations(db) {
-  try {
-    db.exec('ALTER TABLE Users ADD COLUMN two_fa_code TEXT');
-  } catch (err) {}
-  try {
-    db.exec('ALTER TABLE Users ADD COLUMN two_fa_expires DATETIME');
-  } catch (err) {}
-  try {
-    db.exec('ALTER TABLE Users ADD COLUMN reset_token TEXT');
-  } catch (err) {}
-  try {
-    db.exec('ALTER TABLE Users ADD COLUMN reset_token_expires DATETIME');
-  } catch (err) {}
+  try { db.exec('ALTER TABLE Users ADD COLUMN two_fa_code TEXT'); } catch (err) {}
+  try { db.exec('ALTER TABLE Users ADD COLUMN two_fa_expires DATETIME'); } catch (err) {}
+  try { db.exec('ALTER TABLE Users ADD COLUMN reset_token TEXT'); } catch (err) {}
+  try { db.exec('ALTER TABLE Users ADD COLUMN reset_token_expires DATETIME'); } catch (err) {}
 }
 
-const dbPath = path.resolve(__dirname, '..', 'data', 'BRIGHTDatabase.db');
+const VOLUME_PATH = '/app/server/data/BRIGHTDatabase.db';
+const LOCAL_PATH = path.resolve(__dirname, '..', 'data', 'BRIGHTDatabase.db');
+const dbPath = fs.existsSync('/app/server/data') ? VOLUME_PATH : LOCAL_PATH;
+
 console.log('Final Database Path:', dbPath);
 
 const db = new Database(dbPath);
